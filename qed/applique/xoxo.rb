dd01d2c8a8184cafdb94db9713dd6b82118df397
7rans <transfire@gmail.com>
2011-10-26 12:27:40 -0400

    Hash order is only preserved in Ruby 1.9 and up. [test]

--- qed/applique/xoxo.rb
+++ qed/applique/xoxo.rb
@@ -1,6 +1,14 @@
 require 'xoxo'
 
+When "On Ruby 1.9, serializes to" do |text|
+  if RUBY_VERSION >= '1.9'
+    text = text.gsub(/^\s+/, '').gsub(/\n/, '')
+    @_.assert == text 
+  end
+end
+
 When "serializes to" do |text|
+p text
   text = text.gsub(/^\s+/, '').gsub(/\n/, '')
   @_.assert == text 
 end
