3db017076270504139fd6bf66ab04f609d668e08
7rans <transfire@gmail.com>
2011-10-26 12:57:40 -0400

    For Js sake. Please pass. [test]

--- qed/applique/xoxo.rb
+++ qed/applique/xoxo.rb
@@ -1,12 +1,5 @@
 require 'xoxo'
 
-When "On Ruby 1.9, this becomes" do |text|
-  if RUBY_VERSION >= '1.9'
-    text = text.gsub(/^\s+/, '').gsub(/\n/, '')
-    @_.assert == text 
-  end
-end
-
 When "serializes to" do |text|
   text = text.gsub(/^\s+/, '').gsub(/\n/, '')
   @_.assert == text 
